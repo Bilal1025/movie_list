9e64060422bf5d616eb6093854f485f5e6a727a5
Bilal <mbilal1980@hotmail.com>
2022-02-09 11:05:40 +0500

    Added Antd table to display movies

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+import { Table } from "antd";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [ dataSource, setDataSource ] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/v1/movies')
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setDataSource(result)
+        },
+        (error) => {
+          console.log(error)
+        }
+      )
+  }, [])
+
+  const columns = [
+    {
+      title: 'Title',
+      dataIndex: 'title',
+      key: 'title',
+    },
+    {
+      title: 'Genre',
+      dataIndex: 'genre',
+      key: 'genre',
+    },
+    {
+      title: 'Year',
+      dataIndex: 'year',
+      key: 'year',
+    },
+    {
+      title: 'Rating',
+      dataIndex: 'rating',
+      key: 'rating',
+    },
+    {
+      title: 'Expert Rating',
+      dataIndex: 'expert_rating',
+      key: 'expert_rating',
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table dataSource={ dataSource } columns={ columns }/>;
     </div>
   );
 }
